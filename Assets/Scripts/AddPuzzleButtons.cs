96e85364d6acf625f5a4d136d25d25de99f1a791
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-05-19 10:29:35 +0500

    Instantiating Puzzle Buttons Through Script

--- Assets/Scripts/AddPuzzleButtons.cs
+++ Assets/Scripts/AddPuzzleButtons.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class AddPuzzleButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] 
+    private GameObject puzzleButtonPrefab;
+    [SerializeField]
+    private Transform puzzleFieldTransform;
+   void Awake(){
+    for(int i=0;i<8;i++){
+    GameObject puzzleButton = Instantiate(puzzleButtonPrefab);
+    puzzleButton.name = "" + i;
+    puzzleButton.transform.SetParent(puzzleFieldTransform,false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   }
 }
